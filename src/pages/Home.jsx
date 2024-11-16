69b00e0ae54dbd4f94ad4c7bae989d010313b365
Matthew McNee <matt@acyleris.com>
2024-11-16 13:37:06 +0000

    added border to linkedin posts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,8 +8,8 @@ const Home = () => {
             <div className="homepage-banner"> </div>
             <h1>Welcome to the Home Page</h1>
             <ChatboxOverlay />
-            <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7253018009873608704" height="848" width="504" frameBorder="0" allowFullScreen="" title="Embedded post"></iframe>
-            <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7245151802453594113" height="787" width="504" frameBorder="0" allowFullScreen="" title="Embedded post"></iframe>
+            <iframe className='linkedin-embed' src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7253018009873608704" height="848" width="504" frameBorder="0" allowFullScreen="" title="Embedded post"></iframe>
+            <iframe className='linkedin-embed' src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7245151802453594113" height="787" width="504" frameBorder="0" allowFullScreen="" title="Embedded post"></iframe>
         </div>
     );
 };
