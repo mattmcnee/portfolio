0f066840f053b6228072c19ae451eb584e1de6e2
Matthew McNee <matt@acyleris.com>
2024-11-16 13:34:01 +0000

    added fullheight landing screen to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,50 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Home from '/src/pages/Home';
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const pageRef = useRef(null);
+
+  // 1vh fix for mobile browsers
+  useEffect(() => {
+    const updateViewportHeight = () => {
+      const windowHeight = window.innerHeight;
+      document.documentElement.style.setProperty('--vh', `${windowHeight * 0.01}px`);
+      if (window.scrollY === 0 || window.innerWidth > 768) {
+        document.documentElement.style.setProperty('--vh-page-top-only', `${windowHeight * 0.01}px`);
+      }   
+    }; 
+    updateViewportHeight();
+
+    const resizeObserver = new ResizeObserver(() => {
+      updateViewportHeight();
+    });
+
+    if (pageRef.current) {
+      resizeObserver.observe(pageRef.current);
+    }
+
+    window.addEventListener('scroll', updateViewportHeight); 
+    window.addEventListener('resize', updateViewportHeight);
+
+    return () => {
+      if (pageRef.current) {
+        resizeObserver.unobserve(pageRef.current);
+      }
+      window.removeEventListener('scroll', updateViewportHeight);
+      window.removeEventListener('resize', updateViewportHeight);
+    };
+  }, [pageRef]);  
 
   return (
-    <Router>
-      <Routes>
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </Router>
+    <div id="page" ref={pageRef}>
+      <Router>
+        <Routes>
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ChatboxOverlay from '/src/components/ChatboxOverlay';
+import './Home.scss';
 
 const Home = () => {
     return (
         <div>
+            <div className="homepage-banner"> </div>
             <h1>Welcome to the Home Page</h1>
             <ChatboxOverlay />
-            <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7253018009873608704" height="848" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
-            <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7245151802453594113" height="787" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
+            <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7253018009873608704" height="848" width="504" frameBorder="0" allowFullScreen="" title="Embedded post"></iframe>
+            <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7245151802453594113" height="787" width="504" frameBorder="0" allowFullScreen="" title="Embedded post"></iframe>
         </div>
     );
 };
