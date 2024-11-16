293fe2d5135623f3db6faec274850129857829bc
Matthew McNee <matt@acyleris.com>
2024-11-16 12:41:37 +0000

    removed vite default styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 import Home from '/src/pages/Home';
 
 const App = () => {
